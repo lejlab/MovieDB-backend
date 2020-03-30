6c734e5d83f8e60610c4546d2b964ed469d4772b
unknown <agobeljic1@hotmail.com>
2020-03-30 10:48:08 +0200

    Eureka

--- movieAPI/src/main/java/com/moviedb/Movies/MoviesApplication.java
+++ movieAPI/src/main/java/com/moviedb/Movies/MoviesApplication.java
@@ -9,6 +9,7 @@ import com.moviedb.Movies.repositories.MoviesRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import org.slf4j.Logger;
@@ -18,6 +19,7 @@ import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import java.util.Date;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MoviesApplication {
 	private static final Logger log = LoggerFactory.getLogger(MoviesApplication.class);
 
